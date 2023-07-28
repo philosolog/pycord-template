10bf314c27dcf69ce3eb3d96fdbaf82fcc1d5583
aaron <94033911+philosolog@users.noreply.github.com>
2023-07-28 02:19:48 -0700

    Update utilities.py

--- cogs/utilities.py
+++ cogs/utilities.py
@@ -1,7 +1,3 @@
-# @bot.slash_command(name="ping")
-# async def global_command(ctx: discord.ApplicationContext):
-# 	await ctx.respond(f"# returned in {round(Decimal(bot.latency*1000), 3)}ms")
-
 import discord
 from discord.ext import commands
 from decimal import Decimal
@@ -11,8 +7,8 @@ class utilities(commands.Cog):
         self.bot = bot
 
     @commands.slash_command()
-    async def ping(self, ctx: discord.ApplicationContext):
-        await ctx.respond("# ill add pinging later")
+    async def aslashcommand(self, ctx: discord.ApplicationContext):
+        await ctx.respond("# what a useful slash command")
 
 
 def setup(bot):
