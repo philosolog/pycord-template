fc64bca8fe6e031472133b71d567d5706a85810c
aaron <94033911+philosolog@users.noreply.github.com>
2023-07-28 02:20:53 -0700

    Update and rename utilities.py to template.py

--- cogs/template.py
+++ cogs/template.py
@@ -2,14 +2,14 @@ import discord
 from discord.ext import commands
 from decimal import Decimal
 
-class utilities(commands.Cog):
+class template(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.slash_command()
-    async def aslashcommand(self, ctx: discord.ApplicationContext):
+    async def slash_command(self, ctx: discord.ApplicationContext):
         await ctx.respond("# what a useful slash command")
 
 
 def setup(bot):
-    bot.add_cog(utilities(bot))
+    bot.add_cog(template(bot))
